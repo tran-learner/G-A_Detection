5fbec506d5e66df118bafe78cc3243de8a7b78ee
Kim Tran <lucian457fcde@gmail.com>
2025-04-19 15:27:26 +0700

    1901

--- cameraHandle/getPiCamera.py
+++ cameraHandle/getPiCamera.py
@@ -1,34 +1,47 @@
-from picamera.array import PiRGBArray
-from picamera import PiCamera
-import imutils
+from picamera2 import Picamera2
+
 import time
 import cv2
+import os
+import sys
+sys.path.append(os.path.abspath("../G&A_DETECTION/"))
+from utils.ga_model_handle import predict_ga
 
-camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(640,480))
-
-MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
-
-time.sleep(0.1)
 def capture_loop():
-    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-        image = frame.array
-        face_cascade = cv2.CascadeClassifier('../assets/haarcascade_frontalface_alt.xml')
-        
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray,1.1,5)
-        print("found "+str(len(faces))+"faces")
-        
+    face_cascade = cv2.CascadeClassifier('assets/haarcascade_frontalface_alt.xml')
+    
+    frame_count = 0
+    process_every_n_frames =30
+    
+    camera = PiCamera2()
+    config = camera.create_preview_configuration(main={"format":"RGB888", "size":(700,500)})
+    camera.configure(config)
+    camera.start()
+    time.sleep(1)
+
+
+    while (True):
+        frame = camera.capture_array()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=7, minSize=(30, 30))
         for (x,y,w,h) in faces:
-            cv2.rectangle(image, (x,y),(x+w,y+h),(255,255,0),2)
+            cv2.rectangle(frame, (x,y),(x+w,y+h),(255,255,0),3)
+            if frame_count % process_every_n_frames ==0:
+                face_img = frame[y:y+h, x:x+w].copy()
+                age, gender = predict_ga(face_img)
+                # print(a)
+                
+            label = f"{gender}, {age}"
+            cv2.putText(frame, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX,0.7, (0, 255, 0), 2)
+        frame_count +=1
+        if (frame_count>325):
+            frame_count=0
         
-        cv2.imshow("OK",image)
-        key = cv2.waitKey(1) & 0xFF
+        label = f"{gender}, {age}"        
         
-        rawCapture.truncate(0)
-        if key== ord("q"):
+        cv2.imshow("camera",frame)
+        if cv2.waitKey(1)==ord("q"):
             break
-        
-capture_loop()
+    cv2.destroyAllWindows()
+    camera.stop()
+
